857769f378a475fc59a0b4dfffeed6d0e6d8461f
Qps-java <qps200107@gmail.com>
2024-09-18 18:54:52 +0800

    这是我的第二个测试 1

---
+++ src/main/java/com/qps/Hello.java
@@ -0,0 +1,12 @@
+package com.qps;
+
+/**
+ * @Description TODO
+ * @Author 程序员-邱朋帅
+ * @Date 2024/9/18 18:53
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
